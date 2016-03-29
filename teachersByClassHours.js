e9395ad94c6125b1dccd8464709e18e001b5ea6b
Ranieri Althoff <ranisalt@gmail.com>
2016-03-29 20:45:19 -0300

    Add scripts to rank teachers by class amount & time

---
+++ teachersByClassHours.js
@@ -0,0 +1,26 @@
+// configuration
+var database = "moita"; // database name
+var output = "teachersByClassHours"; // output collection name (for shell usage)
+var semester = "20161"; // desired semester
+
+// code
+db = db.getSiblingDB(database);
+
+var map = function() {
+  for (var i in this.classes) {
+    var class_ = this.classes[i];
+    for (var j in class_.teachers) {
+      var teacher = class_.teachers[j];
+      emit(teacher, this.hours / 18);
+    }
+  }
+};
+
+var reduce = function(key, values) {
+  return Array.sum(values);
+};
+
+var options = { query: { semester: semester }, out: output };
+
+db.moita.mapReduce(map, reduce, options);
+db[output].find().sort({ value: -1 }).limit(20).forEach(printjson);
