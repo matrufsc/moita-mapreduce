74bcc987e03666db2a455073d089ed7ca71a9575
Ranieri Althoff <ranisalt@gmail.com>
2016-03-29 20:57:56 -0300

    Add scripts to rank subjects by occupied/remaining vacancy

---
+++ totalRemainingVacancyBySubject.js
@@ -0,0 +1,23 @@
+// configuration
+var database = "moita"; // database name
+var output = "totalRemainingVacancyBySubject"; // output collection name (for shell usage)
+var semester = "20161"; // desired semester
+
+// code
+db = db.getSiblingDB(database);
+
+var map = function() {
+  for (var i in this.classes) {
+    var class_ = this.classes[i];
+    emit(this._id, class_.vacancy - (class_.occupied + class_.special));
+  }
+};
+
+var reduce = function(key, values) {
+  return Array.sum(values);
+};
+
+var options = { query: { semester: semester }, out: output };
+
+db.moita.mapReduce(map, reduce, options);
+db[output].find().sort({ value: -1 }).limit(20).forEach(printjson);
